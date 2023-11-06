ec42c9219eee219f2330be942c435974b05d59bf
1vel <137499479+Leviatan1121@users.noreply.github.com>
2023-11-06 09:10:21 +0100

    redundant code removed

--- dist/child_runner.cjs
+++ dist/child_runner.cjs
@@ -1,17 +1,17 @@
 const { exec } = require('child_process');
 
-const runChild = async (file) => {
-    const runFile = (file) => {
-        return new Promise((resolve, reject) => {
-            exec(`node ${file}`, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    reject(error);
-                } else resolve(stdout);
-            });
+const runFile = (file) => {
+    return new Promise((resolve, reject) => {
+        exec(`node ${file}`, (error, stdout) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                reject(error);
+            } else resolve(stdout);
         });
-    };
+    });
+};
 
+const runChild = async (file) => {
     try {
         const output = await runFile(file);
         console.log(output);
@@ -20,18 +20,7 @@ const runChild = async (file) => {
     }
 };
 
-async function runChildren(files) {
-    const runFile = (file) => {
-        return new Promise((resolve, reject) => {
-            exec(`node ${file}`, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    reject(error);
-                } else resolve(stdout);
-            });
-        });
-    };
-    
+const runChildren = async (files) => {
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
         try {
